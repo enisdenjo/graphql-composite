7c9a095fdb45f2ed13009f56194098897e6ca553
enisdenjo <denis@denelop.com>
2024-05-16 13:48:24 +0200

    yoga in source

--- tests/utils.ts
+++ tests/utils.ts
@@ -1,8 +1,8 @@
 import fs from 'fs/promises';
 import path from 'path';
 import { IExecutableSchemaDefinition } from '@graphql-tools/schema';
-import { DocumentNode, ExecutionResult } from 'graphql';
-import { createSchema, createYoga } from 'graphql-yoga';
+import { DocumentNode, ExecutionResult, GraphQLSchema } from 'graphql';
+import { createSchema, createYoga, YogaServerInstance } from 'graphql-yoga';
 import { SchemaPlan } from '../src/schemaPlan.js';
 
 export type FixtureSources = Record<string, Source>;
@@ -43,16 +43,23 @@ export async function getFixtures(): Promise<Fixture[]> {
 }
 
 export interface Source {
+  yoga: YogaServerInstance<any, any>;
   fetch: typeof fetch;
 }
 
-export function createSource(def: IExecutableSchemaDefinition): Source {
-  const { fetch } = createYoga({
+export function createSource(
+  defOrSchema: IExecutableSchemaDefinition | GraphQLSchema,
+): Source {
+  const yoga = createYoga({
     maskedErrors: false,
-    schema: createSchema(def),
+    schema:
+      defOrSchema instanceof GraphQLSchema
+        ? defOrSchema
+        : createSchema(defOrSchema),
   });
   return {
+    yoga,
     // @ts-expect-error TODO: yoga's `fetch` types dont match the native fetch types.
-    fetch: fetch,
+    fetch: yoga.fetch,
   };
 }

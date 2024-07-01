ae2328ea963871fbd5e290fc9ee7f831993ea023
enisdenjo <denis@denelop.com>
2024-07-01 12:41:39 +0200

    one more test

--- tests/fixtures/variable-selects-from-different-resolver/queries.ts
+++ tests/fixtures/variable-selects-from-different-resolver/queries.ts
@@ -25,4 +25,36 @@ export const queries: FixtureQueries = [
       },
     },
   },
+  {
+    name: 'VariableFromDifferentSubgraphAndAll',
+    document: parse(/* GraphQL */ `
+      query VariableFromDifferentSubgraphAndAll {
+        products {
+          id
+          upc
+          name
+          price
+        }
+      }
+    `),
+    variables: {},
+    result: {
+      data: {
+        products: [
+          {
+            id: '1',
+            upc: 'h1',
+            name: 'Hazelnut',
+            price: 10,
+          },
+          {
+            id: '2',
+            upc: 's2',
+            name: 'Sunflower Seeds',
+            price: 14,
+          },
+        ],
+      },
+    },
+  },
 ];

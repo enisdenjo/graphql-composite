cc66f742a4660a4d75904461bb90ee240dfe5e42
enisdenjo <denis@denelop.com>
2024-09-04 16:08:02 +0200

    dedebug

--- tests/planGatherAndExecute.test.ts
+++ tests/planGatherAndExecute.test.ts
@@ -28,10 +28,6 @@ describe.each(await getFixtures())(
           expect({ errors }).toEqual(result);
           return;
         }
-        const p = planGather(blueprint, document);
-
-        console.dir(p, { depth: 30 });
-
         const { extensions, ...actualResult } = await execute(
           Object.entries(subgraphs).reduce(
             (agg, [name, subgraph]) => ({
@@ -40,7 +36,7 @@ describe.each(await getFixtures())(
             }),
             {},
           ),
-          p,
+          planGather(blueprint, document),
           variables,
         );
         expect(actualResult).toEqual(result);
